ab3b1269342d22e9526941af6816c709d6c401e5
nescio007 <nescio007@users.noreply.github.com>
2021-01-22 18:08:58 +0100

    bump version
    bump version after fix in forward.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='teether',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
     install_requires=[
         'z3-solver>=4.8.5.0',
