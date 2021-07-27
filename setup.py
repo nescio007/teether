04adf568b5dcaf9dba0c655e3f234d19764cb603
Johannes Krupp <johannes.krupp@cispa.saarland>
2021-07-27 12:49:38 +0200

    bump version to 0.1.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='teether',
-    version='0.1.9',
+    version='0.1.10',
     packages=find_packages(),
     install_requires=[
         'z3-solver>=4.8.10.0',
